b3df6fe788678a0a115cafaf98d315c7e5c26fba
cono200 <tatattian04@gmail.com>
2024-01-27 23:32:13 -0700

    trigger SIIUUUUUUUUU

---
+++ miniProyectoNotas_SLE/Trigger/RandomColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace miniProyectoNotas_SLE.Trigger
+{
+    public class RandomColorConverter : IValueConverter
+    {
+        private static readonly Random random = new Random();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Color.FromRgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
